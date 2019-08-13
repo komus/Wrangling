6e523c62d6c3b52087ae01de635abccbfa95046f
Oyindolapo <oyindolapokomolafe@yahoo.com>
2019-08-13 21:30:39 +0100

    string pasrsing addition

---
+++ stringParsing.R
@@ -0,0 +1,26 @@
+library(dslabs)
+data(reported_heights)
+
+library(stringr)
+
+schools <- c("U. Kentucky" , "Univ New Hampshire", "Univ. of Massachusetts", "University Georgia", "U California" ,"California State University")
+
+schools %>% 
+  str_replace("^Univ\\.?\\s|^U\\.?\\s", "University ") %>% 
+  str_replace("^University of |^University ", "University of ")
+
+problems <- c("5.3", "5,5", "6 1", "5 .11", "5, 12")
+pattern_with_groups <- "^([4-7])[,\\.](\\d*)$"
+str_replace(problems, pattern_with_groups, "\\1'\\2")
+
+yes <- c("5 feet 7inches", "5 7")
+no <- c("5ft 9 inches", "5 ft 9 inches")
+s <- c(yes, no)
+
+converted <- s %>% 
+  str_replace("\\s*(feet|foot|ft)\\s*", "'") %>% 
+  str_replace("\\s*(inches|in|''|\")\\s*", "") %>% 
+  str_replace("^([4-7])\\s*[,\\.\\s+]\\s*(\\d*)$", "\\1'\\2")
+
+pattern <- "^[4-7]\\s*'\\s*\\d{1,2}$"
+str_detect(converted, pattern)
